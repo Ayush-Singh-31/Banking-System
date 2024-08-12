583d8cf674a64a79b29c31f9c984a750b8d3d08c
Ayush <sayush5191@gmail.com>
2024-08-12 16:23:45 +0530

    Calculator Update

--- Calculator/Main.c
+++ Calculator/Main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(){
-    printf("Hello World\n");
+int main() {
+    int a, b;
+    char op;
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+    printf("Enter the operator: ");
+    scanf(" %c", &op);
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+    switch(op) {
+        case '+':
+            printf("The sum is %d\n", a + b);
+            break;
+        case '-':
+            printf("The difference is %d\n", a - b);
+            break;
+        case '*':
+            printf("The product is %d\n", a * b);
+            break;
+        case '/':
+            printf("The quotient is %d\n", a / b);
+            break;
+        default:
+            printf("Invalid operator\n");
+    }
     return 0;
 }
